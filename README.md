63ef769da95bff63bf09578423a3a1c968d97acc
Nick Maludy <nmaludy@gmail.com>
2018-07-25 21:40:08 -0400

    Added initial actions

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # bolt Integration Pack
 StackStorm integration pack for Puppet Bolt
 
+# TODO
+* This readme
+* Tests
+* Config (what options do we put in there?)
+* Action to install bolt
+
+
 ## Configuration
 TODO: Describe configuration
 
---
+++ actions/lib/bolt.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+from st2common.runners.base_action import Action
+
+import json
+import os
+import six
+from six.moves import shlex_quote
+import subprocess
+import sys
+
+SKIP_ARGS = [
+    'cmd',
+    'sub_command',
+    'cwd',
+    'env'
+]
+
+BOLT_OPTIONS = [
+    'nodes',
+    'query',
+    'noop',
+    'description',
+    'params',
+    'params_obj',
+    'user',
+    'password',
+    'private_key',
+    'host_key_check',
+    'ssl',
+    'ssl_verify',
+    'run_as',
+    'sudo_password',
+    'concurrency',
+    'compile_concurrency',
+    'modulepath',
+    'boltdir',
+    'configfile',
+    'inventoryfile',
+    'transport',
+    'connect_timeout',
+    'tty',
+    'tmpdir',
+    'format',
+    'verbose',
+    '_debug',
+]
+
+class BoltAction(Action):
+
+    def format_option(self, option):
+        # remove leading _ for debug
+        if option[0] == '_':
+            option = option[1:]
+        return option.replace('_', '-')
+
+    def build_args(self, **kwargs):
+        args = []
+        options = []
+
+        # hack to make sure src comes before dest
+        if 'src' in kwargs:
+            args.append(kwargs['src'])
+            del kwargs['src']
+        if 'dest' in kwargs:
+            args.append(kwargs['dest'])
+            del kwargs['dest']
+
+        for k, v in six.iteritems(kwargs):
+            if k in SKIP_ARGS:
+                continue
+            if v is None:
+                continue
+
+            if k in BOLT_OPTIONS:
+                k_formatted = self.format_option(k)
+                if isinstance(v, bool):
+                    if v:
+                        options.append('--{}'.format(k_formatted))
+                    else:
+                        options.append('--no-{}'.format(k_formatted))
+                else:
+                    options.append('--{}'.format(k_formatted))
+                    options.append(v)
+            else:
+                args.append(v)
+        return args, options
+
+    def execute(self, cmd, sub_command, env, cwd, args, options):
+        full_cmd = [cmd] + sub_command.split(' ') + args + options
+
+        process = subprocess.Popen(full_cmd,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE,
+                                   env=env,
+                                   cwd=cwd)
+        stdout, stderr = process.communicate()
+        if stdout:
+            try:
+                stdout = json.loads(stdout)
+            except Exception as e:
+                self.logger.exception(e)
+
+        if stderr:
+            try:
+                stderr = json.loads(stderr)
+            except:
+                pass
+            sys.stderr.write(stderr)
+
+        return_code = process.poll()
+        success = False if return_code else True
+        return (success, stdout)
+
+    def run(self, **kwargs):
+        cmd = kwargs['cmd']
+        sub_command = kwargs['sub_command']
+        env = os.environ.copy()
+        env.update(kwargs.get('env', {}))
+        cwd = kwargs.get('cwd', None)
+
+        args, options = self.build_args(**kwargs)
+        return self.execute(cmd, sub_command, env, cwd, args, options)
